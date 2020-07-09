6062c4d9782d8aaf814ec9cf686f27dd3600df9d
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-07-09 23:15:58 +0530

    To convert  temperature in celsius and fahrenheit

---
+++ temperature.py
@@ -0,0 +1,6 @@
+celsius=float(input("Enter the temperature in celsius:"))
+fahrenheit=(celsius*1.8)+32
+print('%.2f Celsius is %0.2f Fahrenheit' %(celsius,fahrenheit))
+fahrenheit=float(input("Enter the temperature in fahrenheit:"))
+celsius=(fahrenheit-32)/1.8
+print('%.2f Fahrenheit is %0.2f Celsius' %(fahrenheit,celsius))
