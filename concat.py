8b2bfafd6de10da3867ddb5ac7b3820bab552422
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-07-30 22:38:21 +0530

    The program to concatenate two strings

---
+++ concat.py
@@ -0,0 +1,8 @@
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+ 
+c1 = string1 + string2
+c2 = string1 + ' ' + string2
+
+print("string concatenation =",c1)
+print("string concatenation with space =",c2)
