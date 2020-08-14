3fa6205f1b03ca7e90ff1b3a1963db79b72e672f
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-08-14 23:53:33 +0530

    To find the circumference and area of a circle

---
+++ circle_area.py
@@ -0,0 +1,7 @@
+PIE = 3.14
+radius = float(input("Enter the radius of a circle: "))
+area = PIE * radius * radius
+circumference = 2 * PIE * radius
+
+print(" Area Of a Circle = %.2f" %area)
+print(" Circumference Of a Circle = %.2f" %circumference)
