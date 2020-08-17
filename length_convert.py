d8cd367b1547755f10dc962999f3f64e9b13a461
Sudhapraba <66582698+Sudhapraba@users.noreply.github.com>
2020-08-17 23:11:10 +0530

    The program to displays length conversion of feets

---
+++ length_convert.py
@@ -0,0 +1,9 @@
+feet = int(input("Enter the value in feet:"))
+
+inches = feet * 12
+yard = feet / 3.0
+mile = feet / 5280.0
+
+print("Distance in inches is %d " % inches)
+print("Distance in yard is %0.2f " % yard)
+print("Distance in mile is %0.2f " % mile)
